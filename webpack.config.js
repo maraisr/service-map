ecf4ac988b544b0040ca8fb9d1c829ae15e8400f
Marais Rossouw <me@maraisr.com>
2018-05-27 18:36:03 +1000

    chore: auto formatted

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,50 @@ const { join } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+	context: join(__dirname, 'src/public'),
 
-    context: join(__dirname, 'src/public'),
+	entry: {
+		main: './index.tsx'
+	},
 
-    entry: {
-        main: './index.tsx'
-    },
+	output: {
+		filename: '[name].[hash].bundle.js',
+		path: join(__dirname, 'dist/public')
+	},
 
-    output: {
-        filename: "[name].[hash].bundle.js",
-        path: join(__dirname, 'dist/public')
-    },
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.json']
+	},
 
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/, use: [
-                    {
-                        loader: 'babel-loader'
-                    },
-                    {
-                        loader: 'awesome-typescript-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: 'html-loader',
-                    options: {
-                        minimize: true
-                    }
-                }],
-            }
-        ]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './index.html'
-    })]
-}
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: [
+					{
+						loader: 'babel-loader'
+					},
+					{
+						loader: 'awesome-typescript-loader'
+					}
+				]
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: {
+							minimize: true
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html'
+		})
+	]
+};
